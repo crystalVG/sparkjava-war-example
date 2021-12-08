ae3c22d898ca8733d79d7e7b3d1f817afccd9f18
crystalVG <95430246+crystalVG@users.noreply.github.com>
2021-12-08 11:29:03 -0600

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hey its Crystal! yeih!!");
+		get("/hello", (req, res) -> "Hello everyone");
 	}
 }
